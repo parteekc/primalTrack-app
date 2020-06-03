35355e1a6baaf5b8018aa759fb6c5c10e6004c80
Mudit Mittal <mmudit30@gmail.com>
2020-06-04 00:23:18 +0530

    connection component added

---
+++ src/app/connection/connection.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connection',
+  templateUrl: './connection.component.html',
+  styleUrls: ['./connection.component.css']
+})
+export class ConnectionComponent implements OnInit {
+
+  constructor() { 
+    document.title= "Connection";
+  }
+
+  ngOnInit() {
+  }
+
+}
