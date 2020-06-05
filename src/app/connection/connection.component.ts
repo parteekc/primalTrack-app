1e838ebd79df47ac64c85adf912481437ba8290c
Mudit Mittal <mmudit30@gmail.com>
2020-06-05 16:44:30 +0530

    added back button

--- src/app/connection/connection.component.ts
+++ src/app/connection/connection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-connection',
@@ -7,11 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnectionComponent implements OnInit {
 
-  constructor() { 
+  constructor(public location: Location ) { 
     document.title= "Connection";
   }
 
   ngOnInit() {
+    console.log("on init connection");
   }
 
 }
