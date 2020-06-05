08fb239d82908b7acc86681062904e4118ca70b3
Mudit Mittal <mmudit30@gmail.com>
2020-06-05 16:44:43 +0530

    added back button

--- src/app/health-report/health-report.component.ts
+++ src/app/health-report/health-report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-health-report',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HealthReportComponent implements OnInit {
 
-  constructor() { 
+  constructor(public location : Location) { 
     document.title = "Report";
   }
 
