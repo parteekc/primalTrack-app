f56b844832849897e8c0ba462bf733ab06746552
Mudit Mittal <mmudit30@gmail.com>
2020-06-05 23:06:20 +0530

    added firebase, AngularFir, authentication files

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+import { AuthService} from './auth.service'
+import { Observable } from 'rxjs';
+import { tap, map, take } from 'rxjs/operators';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+
+      return this.auth.user$.pipe(
+           take(1),
+           map(user => !!user), // <-- map to boolean
+           tap(loggedIn => {
+             if (!loggedIn) {
+               console.log('access denied')
+               this.router.navigate(['/signin']);
+             }
+         })
+    )
+  }
+}
