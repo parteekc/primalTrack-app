0faf16dd3275f94b005be6a928797496b0ae2a20
Mudit Mittal <mmudit30@gmail.com>
2020-06-09 22:48:52 +0530

    dummy json data for chart added

---
+++ src/assets/json/sys_dia.ts
@@ -0,0 +1,412 @@
+export const sys_dia =  [
+   {
+      "year": 1975,
+      "systolic": 126.6395165,
+      "diastolic": 78.7455078
+   },
+   {
+      "year": 1976,
+      "systolic": 126.616241,
+      "diastolic": 78.73865109
+   },
+   {
+      "year": 1977,
+      "systolic": 126.5923775,
+      "diastolic": 78.73194307
+   },
+   {
+      "year": 1978,
+      "systolic": 126.5667763,
+      "diastolic": 78.72496051
+   },
+   {
+      "year": 1979,
+      "systolic": 126.5394621,
+      "diastolic": 78.7178488
+   },
+   {
+      "year": 1980,
+      "systolic": 126.5119416,
+      "diastolic": 78.71189554
+   },
+   {
+      "year": 1981,
+      "systolic": 126.4829406,
+      "diastolic": 78.70719978
+   },
+   {
+      "year": 1982,
+      "systolic": 126.4510429,
+      "diastolic": 78.70222599
+   },
+   {
+      "year": 1983,
+      "systolic": 126.4147171,
+      "diastolic": 78.69619614
+   },
+   {
+      "year": 1984,
+      "systolic": 126.3739833,
+      "diastolic": 78.68888282
+   },
+   {
+      "year": 1985,
+      "systolic": 126.3314181,
+      "diastolic": 78.68228687
+   },
+   {
+      "year": 1986,
+      "systolic": 126.289153,
+      "diastolic": 78.6752731
+   },
+   {
+      "year": 1987,
+      "systolic": 126.24958,
+      "diastolic": 78.67012223
+   },
+   {
+      "year": 1988,
+      "systolic": 126.2129135,
+      "diastolic": 78.66567735
+   },
+   {
+      "year": 1989,
+      "systolic": 126.1788179,
+      "diastolic": 78.66030645
+   },
+   {
+      "year": 1990,
+      "systolic": 126.1485303,
+      "diastolic": 78.65474202
+   },
+   {
+      "year": 1991,
+      "systolic": 126.122569,
+      "diastolic": 78.64881634
+   },
+   {
+      "year": 1992,
+      "systolic": 126.1031654,
+      "diastolic": 78.64466829
+   },
+   {
+      "year": 1993,
+      "systolic": 126.0873059,
+      "diastolic": 78.64206576
+   },
+   {
+      "year": 1994,
+      "systolic": 126.0748904,
+      "diastolic": 78.64027191
+   },
+   {
+      "year": 1995,
+      "systolic": 126.0682489,
+      "diastolic": 78.64156522
+   },
+   {
+      "year": 1996,
+      "systolic": 126.0671215,
+      "diastolic": 78.64523855
+   },
+   {
+      "year": 1997,
+      "systolic": 126.0745203,
+      "diastolic": 78.65160602
+   },
+   {
+      "year": 1998,
+      "systolic": 126.0900864,
+      "diastolic": 78.6608571
+   },
+   {
+      "year": 1999,
+      "systolic": 126.1134131,
+      "diastolic": 78.67105516
+   },
+   {
+      "year": 2000,
+      "systolic": 126.1422295,
+      "diastolic": 78.67992127
+   },
+   {
+      "year": 2001,
+      "systolic": 126.1783161,
+      "diastolic": 78.68813576
+   },
+   {
+      "year": 2002,
+      "systolic": 126.2235944,
+      "diastolic": 78.69609418
+   },
+   {
+      "year": 2003,
+      "systolic": 126.2778009,
+      "diastolic": 78.70226543
+   },
+   {
+      "year": 2004,
+      "systolic": 126.3374691,
+      "diastolic": 78.70639775
+   },
+   {
+      "year": 2005,
+      "systolic": 126.4029808,
+      "diastolic": 78.71001101
+   },
+   {
+      "year": 2006,
+      "systolic": 126.4747342,
+      "diastolic": 78.71362695
+   },
+   {
+      "year": 2007,
+      "systolic": 126.5486472,
+      "diastolic": 78.71580694
+   },
+   {
+      "year": 2008,
+      "systolic": 126.6208193,
+      "diastolic": 78.71605335
+   },
+   {
+      "year": 2009,
+      "systolic": 126.6879136,
+      "diastolic": 78.71439185
+   },
+   {
+      "year": 2010,
+      "systolic": 126.7472192,
+      "diastolic": 78.71005964
+   },
+   {
+      "year": 2011,
+      "systolic": 126.8003423,
+      "diastolic": 78.70537432
+   },
+   {
+      "year": 2012,
+      "systolic": 126.8496818,
+      "diastolic": 78.69983182
+   },
+   {
+      "year": 2013,
+      "systolic": 126.896141,
+      "diastolic": 78.69316511
+   },
+   {
+      "year": 2014,
+      "systolic": 126.9386722,
+      "diastolic": 78.68493173
+   },
+   {
+      "year": 2015,
+      "systolic": 126.97809,
+      "diastolic": 78.67531269
+   },
+   {
+      "year": 1975,
+      "systolic": 123.9306735,
+      "diastolic": 77.47296127
+   },
+   {
+      "year": 1976,
+      "systolic": 123.8817659,
+      "diastolic": 77.43253296
+   },
+   {
+      "year": 1977,
+      "systolic": 123.8312829,
+      "diastolic": 77.39176851
+   },
+   {
+      "year": 1978,
+      "systolic": 123.7789223,
+      "diastolic": 77.34965799
+   },
+   {
+      "year": 1979,
+      "systolic": 123.7253261,
+      "diastolic": 77.30611056
+   },
+   {
+      "year": 1980,
+      "systolic": 123.6716921,
+      "diastolic": 77.26119843
+   },
+   {
+      "year": 1981,
+      "systolic": 123.6198973,
+      "diastolic": 77.21666921
+   },
+   {
+      "year": 1982,
+      "systolic": 123.5681371,
+      "diastolic": 77.17126115
+   },
+   {
+      "year": 1983,
+      "systolic": 123.5147285,
+      "diastolic": 77.12456035
+   },
+   {
+      "year": 1984,
+      "systolic": 123.4580024,
+      "diastolic": 77.07691273
+   },
+   {
+      "year": 1985,
+      "systolic": 123.4007959,
+      "diastolic": 77.03065187
+   },
+   {
+      "year": 1986,
+      "systolic": 123.3451853,
+      "diastolic": 76.98810324
+   },
+   {
+      "year": 1987,
+      "systolic": 123.2927407,
+      "diastolic": 76.95160977
+   },
+   {
+      "year": 1988,
+      "systolic": 123.2410471,
+      "diastolic": 76.91808082
+   },
+   {
+      "year": 1989,
+      "systolic": 123.1885382,
+      "diastolic": 76.88605375
+   },
+   {
+      "year": 1990,
+      "systolic": 123.134347,
+      "diastolic": 76.85484175
+   },
+   {
+      "year": 1991,
+      "systolic": 123.078801,
+      "diastolic": 76.8244938
+   },
+   {
+      "year": 1992,
+      "systolic": 123.0210441,
+      "diastolic": 76.79536447
+   },
+   {
+      "year": 1993,
+      "systolic": 122.9561796,
+      "diastolic": 76.76746074
+   },
+   {
+      "year": 1994,
+      "systolic": 122.8847612,
+      "diastolic": 76.74023515
+   },
+   {
+      "year": 1995,
+      "systolic": 122.80821,
+      "diastolic": 76.71457139
+   },
+   {
+      "year": 1996,
+      "systolic": 122.7283627,
+      "diastolic": 76.69057794
+   },
+   {
+      "year": 1997,
+      "systolic": 122.6499303,
+      "diastolic": 76.66950012
+   },
+   {
+      "year": 1998,
+      "systolic": 122.5756666,
+      "diastolic": 76.65210134
+   },
+   {
+      "year": 1999,
+      "systolic": 122.5065976,
+      "diastolic": 76.63751056
+   },
+   {
+      "year": 2000,
+      "systolic": 122.4431475,
+      "diastolic": 76.62365224
+   },
+   {
+      "year": 2001,
+      "systolic": 122.3868749,
+      "diastolic": 76.61283661
+   },
+   {
+      "year": 2002,
+      "systolic": 122.3409911,
+      "diastolic": 76.6054869
+   },
+   {
+      "year": 2003,
+      "systolic": 122.3064452,
+      "diastolic": 76.60129061
+   },
+   {
+      "year": 2004,
+      "systolic": 122.2843822,
+      "diastolic": 76.60049623
+   },
+   {
+      "year": 2005,
+      "systolic": 122.2737336,
+      "diastolic": 76.60188888
+   },
+   {
+      "year": 2006,
+      "systolic": 122.2749258,
+      "diastolic": 76.60585125
+   },
+   {
+      "year": 2007,
+      "systolic": 122.2828399,
+      "diastolic": 76.61231956
+   },
+   {
+      "year": 2008,
+      "systolic": 122.2926649,
+      "diastolic": 76.62033327
+   },
+   {
+      "year": 2009,
+      "systolic": 122.3012618,
+      "diastolic": 76.63079353
+   },
+   {
+      "year": 2010,
+      "systolic": 122.3049588,
+      "diastolic": 76.64260818
+   },
+   {
+      "year": 2011,
+      "systolic": 122.3036517,
+      "diastolic": 76.65670258
+   },
+   {
+      "year": 2012,
+      "systolic": 122.2997008,
+      "diastolic": 76.67033387
+   },
+   {
+      "year": 2013,
+      "systolic": 122.2924821,
+      "diastolic": 76.68321043
+   },
+   {
+      "year": 2014,
+      "systolic": 122.2800959,
+      "diastolic": 76.69389365
+   },
+   {
+      "year": 2015,
+      "systolic": 122.2633374,
+      "diastolic": 76.70328175
+   }
+]
